4e13d9859bd802f67ea6f1e6986159915ced3298
LeandroAcosta <leacosta97@gmail.com>
2018-01-20 13:31:59 -0300

    hidden y views.py

--- esquelvende/users/views.py
+++ esquelvende/users/views.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+def login_view(request):
+	if request.POST:
+		form = AuthenticationForm(request.POST)
+		if form.is_valid:
+			user_name = request.POST['username']
+			key = request.POST['password']
+			access = authenticate(request, username=user_name, password=key)
+			if access is not None:
+				login(request, access)
+				return HttpResponseRedirect(reverse(""))
+			else:
+				return HttpResponse("error")
+	else:
+		form = AuthenticationForm()
+	return render(request, 'login.html', {'form': form})
