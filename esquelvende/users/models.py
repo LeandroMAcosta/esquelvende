3ef8cb1affd236dcf3547eb0ccd637cfacf31059
LeandroAcosta <leacosta97@gmail.com>
2018-01-25 00:53:12 -0300

    UserProfile y migracion

--- esquelvende/users/models.py
+++ esquelvende/users/models.py
@@ -5,14 +5,9 @@ from django.contrib.auth.models import User
 
 from django.contrib.auth.forms import (UserChangeForm, UserCreationForm)
 
-class UserProfile(User):
-	first_namee = models.CharField(max_length=40, blank=True, null=True)
-	last_namee = models.CharField(max_length=40, blank=True, null=True)
-
-#class Userinfo(models.Model):
-
-	#user = models.OneToOneField(User)
-	#avatar = models.ImageField(upload_to='media/avatar')
-	#phone = models.IntegerField()
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	avatar = models.ImageField(upload_to='media/avatar')
+	phone = models.IntegerField()
 
 
