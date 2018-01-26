d535e8c8b163184bdbd3bab19fc82e2ead61b331
LeandroAcosta <leacosta97@gmail.com>
2018-01-26 01:03:41 -0300

    modelo cambiado

--- esquelvende/users/models.py
+++ esquelvende/users/models.py
@@ -3,8 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-from django.contrib.auth.forms import (UserChangeForm, UserCreationForm)
-
 class UserProfile(models.Model):
 	user = models.OneToOneField(User)
 	avatar = models.ImageField(upload_to='media/avatar')
