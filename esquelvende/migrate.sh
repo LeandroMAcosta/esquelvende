892ae894e3ca4750557c8cdf6c60c2742c61eb26
Lucas de francesca <lucasdfrancesca@gmail.com>
2018-03-01 05:56:34 -0300

    Relleno de base de datos

--- esquelvende/migrate.sh
+++ esquelvende/migrate.sh
@@ -9,3 +9,9 @@ fi
 rm -rf */migrations
 python manage.py makemigrations categories products reports favorites last_seen social_django users notifications
 python manage.py migrate
+
+echo ---------------------------------------
+echo -- Llenando Cateogrias/ Sub/ Filtros --
+echo ---------------------------------------
+
+python manage.py loaddata categories/initial_data/initial.json
