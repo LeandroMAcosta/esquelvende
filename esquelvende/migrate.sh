4bb7f77d20cba17b064af3d71427ae3736b66bd2
Leandro Acosta <leacosta97@gmail.com>
2018-09-07 10:00:39 -0300

    Migrate cambiado

--- esquelvende/migrate.sh
+++ esquelvende/migrate.sh
@@ -6,6 +6,8 @@ if [ -f "db.sqlite3" ]; then
     rm db.sqlite3
 fi
 
+python manage.py flush
+
 rm -rf */migrations
 python manage.py makemigrations categories products reports favorites last_seen social_django users
 python manage.py migrate
