083007a638d47cbd4be739783b31c704ad0871c6
Leandro Acosta <leacosta97@gmail.com>
2019-01-08 15:52:48 -0300

    Add route account/favorites

--- esquelvende/account/urls.py
+++ esquelvende/account/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from .views import edit_user
 
 urlpatterns = [
-    url(r'^perfil/$', edit_user, name='edit_user'),
+    url(r'^account/$', edit_user, name='edit_user'),
+    url(r'^account/favorites/$', favorites),
 ]
