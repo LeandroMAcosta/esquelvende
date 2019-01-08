9432c303c8653227e00730381a9cbd1865d5aa7c
Leandro Acosta <leacosta97@gmail.com>
2019-01-08 18:18:04 -0300

    Fix history route

--- esquelvende/account/urls.py
+++ esquelvende/account/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import edit_user, history
 
 urlpatterns = [
-    url(r'^perfil/$', edit_user, name='edit_user'),
-    url(r'^history/$', history, name='history'),
+    url(r'^account/$', edit_user, name='edit_user'),
+    url(r'^account/history/$', history, name='history'),
 ]
