07804ccb3f759fd6e1b4cf07d3b7c1f9bae4bb84
chesca <joacoemilianodf@gmail.com>
2019-04-16 19:18:01 -0300

    Fix production settings

--- esquelvende/esquelvende/settings/production.py
+++ esquelvende/esquelvende/settings/production.py
@@ -2,4 +2,7 @@ import dj_database_url
 from base import *
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
