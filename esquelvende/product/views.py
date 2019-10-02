1004aad2eededfec3f0d4b210b284d3decfb59e4
chesca <joacoemilianodf@gmail.com>
2019-10-02 16:46:19 -0300

    nothing

--- esquelvende/product/views.py
+++ esquelvende/product/views.py
@@ -96,8 +96,6 @@ def product_detail(request, slug=None, pk=None):
         # TODO: status: Mostrar el estado del producto.
         # TODO: path: Mostrar la ruta de categorias del producto.
     }
-
-
     return render(request, 'view_product.html', context)
 
 
